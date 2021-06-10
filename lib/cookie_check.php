2ab21c4f072e90ba00afafc9a795d263efd3171a
Gavin Pease <gavinpease@gmail.com>
2021-06-10 00:34:14 -0600

    Add url cookie check

--- lib/cookie_check.php
+++ lib/cookie_check.php
@@ -2,4 +2,4 @@
 
 session_start();
 if(!isset($_COOKIE['pimationuseruuid']) || !isset($_COOKIE['loggedIn']) || !isset($_SESSION['UserUUID']))
-    header("Location: /login.php");
+    header("Location: /login.php?url=/modules/SQLSprinkler");
