7176001efa0dfa770f47512b2287d322be9adfa1
Gavin Pease <gavinpease@gmail.com>
2021-06-23 12:33:53 -0600

    Add garage module

--- lib/cookie_check.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-session_start();
-if(!isset($_COOKIE['pimationuseruuid']) || !isset($_COOKIE['loggedIn']) || !isset($_SESSION['UserUUID']))
-    header("Location: /login.php?url=/modules/SQLSprinkler");
--- settings/index.php
+++ settings/index.php
@@ -1,7 +1,7 @@
 <!-- Copyright 2021 Gavin Pease -->
 <?php
 
-include_once ('../lib/cookie_check.php');
+include_once('../lib/cookie_check.php');
 ?>
 <html>
 <head>
